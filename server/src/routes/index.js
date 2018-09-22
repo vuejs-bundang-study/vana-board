9f25a5817d7d223c03c32c1f25ae3b4ea6acbe32
ahribori <ahribori@gmail.com>
2018-09-23 03:38:26 +0900

    CORS 처리

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import { config } from '../constants/config';
 
 const router = express.Router();
+
+const { cors_whitelist } = config.server;
+const whitelist = Array.isArray(cors_whitelist) ? cors_whitelist : [];
+const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true };
+    } else {
+        corsOptions = { origin: false };
+    }
+    callback(null, corsOptions);
+};
+
+router.use('*', cors(corsOptionsDelegate));
 
 router.get('/', (req, res) => {
     res.json('Hello!');
