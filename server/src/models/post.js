5342dc5edd3ae7dfdd9c9f6e819c2f2cf7d63eab
ahribori <ahribori@gmail.com>
2018-09-28 00:34:42 +0900

    레퍼런스 에러 수정

--- server/src/models/post.js
+++ server/src/models/post.js
@@ -6,24 +6,24 @@ const postSchema = new Schema({
     categoryId: String,
     title: { type: String, default: '제목없음' },
     content: String,
-    createdBy: objectId,
+    createdBy: { type: Schema.Types.ObjectId, ref: 'user' },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
     viewCount: { type: Number, default: 0 },
     comments: {
-      type: [
-        {
-          commentSeq: Number,
-          createdBy: String,
-          content: String,
-          isChild: Boolean,
-          parent: Number
-        }
-      ],
-      default: []
+        type: [
+            {
+                commentSeq: Number,
+                createdBy: String,
+                content: String,
+                isChild: Boolean,
+                parent: Number,
+            },
+        ],
+        default: [],
     },
     commentCount: { type: Number, default: 0 },
-    isDeleted: { type: Boolean, default: false }
+    isDeleted: { type: Boolean, default: false },
 });
 
 
