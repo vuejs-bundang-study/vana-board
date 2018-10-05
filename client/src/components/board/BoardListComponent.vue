2343dffcd882f68e3fc8e4a00ff065cee4810d08
sejinpark <psj3205@gmail.com>
2018-10-05 14:56:01 +0900

    메인페이지 제작

--- client/src/components/board/BoardListComponent.vue
+++ client/src/components/board/BoardListComponent.vue
@@ -8,7 +8,7 @@
               <a class="news_title black--text" title="운영알림판 바로가기">운영알림판 ></a>
             </h2>
             <div id = "list" class="list_empty">
-              <span>운영알림판 내용이 없습니다.{{items}}</span>
+              <span>운영알림판 내용이 없습니다.</span>
             </div>
           </v-card>
         </v-flex>
@@ -19,7 +19,7 @@
             </h2>
             <div id = "list">
               <v-list
-                v-for= "item in recommended_items"
+                v-for= "item in recommendedItems"
                 :key= "item.id"
                 :item="item">
                 <a class = "black--text">
@@ -39,7 +39,7 @@
             </h2>
             <div id = "list">
               <v-list
-                v-for= "item in info_items"
+                v-for= "item in infoItems"
                 :key= "item.id"
                 :item="item">
                 <a class = "black--text">
@@ -59,7 +59,7 @@
             </h2>
             <div id = "list">
               <v-list
-                v-for= "item in review_items"
+                v-for= "item in reviewItems"
                 :key= "item.id"
                 :item="item">
                 <a class = "black--text">
@@ -79,7 +79,7 @@
             </h2>
             <div id = "list">
               <v-list
-                v-for= "item in sell_items"
+                v-for= "item in sellItems"
                 :key= "item.id"
                 :item="item">
                 <a class = "black--text">
@@ -96,7 +96,7 @@
             </h2>
             <div id = "list">
               <v-list
-                v-for= "item in promote_items"
+                v-for= "item in promoteItems"
                 :key= "item.id"
                 :item="item">
                 <a class = "black--text">
@@ -104,6 +104,52 @@
                 </a>
               </v-list>
             </div>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card class="section_list gallery text-sm-left">
+            <h2 id= "title" class="section_title">
+              <a class="gallery_title black--text" title="갤러리 바로가기">추천 갤러리 ></a>
+            </h2>
+            <div id= "list"></div>
+            <v-layout row wrap>
+              <v-flex xs6>
+                <v-list
+                  id= "list_gallery"
+                  v-for= "item in galleryItems"
+                  :key= "item.id"
+                  :item= "item"
+                  :src= "item.url">
+                  <a class= "black--text">
+                    <img style = 'height:30%; width:30%;' :src= "item.url">
+                  </a>
+                  <a class= "black--text">
+                    {{item.subject}}
+                  </a>
+                  <a class= "black--text">
+                    {{item.date}}
+                  </a>
+                </v-list>
+              </v-flex>
+              <v-flex xs6>
+                <v-list
+                  id= "list_gallery"
+                  v-for= "item in galleryItems"
+                  :key= "item.id"
+                  :item= "item"
+                  :src= "item.url">
+                  <a class= "black--text">
+                    <img style = 'height:30%; width:30%;' :src= "item.url">
+                  </a>
+                  <a class= "black--text">
+                    {{item.subject}}
+                  </a>
+                  <a class= "black--text">
+                    {{item.date}}
+                  </a>
+                </v-list>
+              </v-flex>
+            </v-layout>
           </v-card>
         </v-flex>
       </v-layout>
@@ -121,188 +167,15 @@ export default {
     this.getAllPosts()
   },
   computed: mapState({
-    items: state => state.post.posts
+    recommendedItems: state => state.post.posts[0].recommendedItems,
+    infoItems: state => state.post.posts[0].infoItems,
+    reviewItems: state => state.post.posts[0].reviewItems,
+    sellItems: state => state.post.posts[0].sellItems,
+    promoteItems: state => state.post.posts[0].promoteItems,
+    galleryItems: state => state.post.posts[0].galleryItems
   }),
   data () {
     return {
-      recommended_items: [
-        {
-          subject: '이재용 레전드.jpg',
-          reply: 65
-        },
-        {
-          subject: '뉴욕에서 벌어진 일본의 졸렬함.jpg',
-          reply: 61
-        },
-        {
-          subject: '어지간하면 야동 지우지마세요',
-          reply: 67
-        },
-        {
-          subject: '벌써 질투받는 97년생 SBS신입 아나운서.jpg',
-          reply: 102
-        },
-        {
-          subject: '문통 워딩도 강하고 시원시원졌네요 ㅋㅋㅋㅋㅋㅋㅋㅋ.gisa',
-          reply: 40
-        },
-        {
-          subject: '(펌) 곧 다가올 그날.jpg',
-          reply: 66
-        },
-        {
-          subject: '문재인 대통령, 미국 Fox News ‘스페셜리포트’ 와 인터뷰',
-          reply: 44
-        },
-        {
-          subject: 'SBS 김수민 아나운서의 과거.jpg',
-          reply: 21
-        },
-        {
-          subject: '통일위해 북한 편? 文대통령 "정권 바뀌니 정반대 비난',
-          reply: 22
-        },
-        {
-          subject: '퀄컴 "애플이 모뎀칩 기밀 빼돌려 인텔에 줬다"',
-          reply: 76
-        },
-        {
-          subject: '에어팟 왼쪽만 팝니다',
-          reply: 7
-        },
-        {
-          subject: '노무현재단 신임 이사장에 유시민 내정(종합)',
-          reply: 23
-        },
-        {
-          subject: '릴레이413일)장충기 삼성공화국과 적폐청산 그날까지!!!',
-          reply: 16
-        }
-      ],
-
-      info_items: [
-        {
-          subject: '엑셀, 모바일 사진 촬영만으로 스프레드시트 변환기능 발표',
-          reply: 4
-        },
-        {
-          subject: '닌텐도 스위치, 클라우드 구독 종료시 데이터 최대 6개월 보존',
-          reply: 42
-        },
-        {
-          subject: '정부, 단속 사각 HTTPS 음란사이트 접속 차단 논의',
-          reply: 27
-        },
-        {
-          subject: '루머) LG, \'V40 ThinQ\' 공식 렌더링 포착',
-          reply: 9
-        },
-        {
-          subject: 'Rtings.com 최고의 TV, 모니터, 헤드폰 리뷰 사이트',
-          reply: 2
-        },
-        {
-          subject: '루머) 구글 \'픽셀 3\' 시리즈, 카메라에 실시간 렌즈 분석 지원',
-          reply: 8
-        },
-        {
-          subject: 'Oracle, Java 11 릴리즈',
-          reply: 17
-        },
-        {
-          subject: 'Apple, macOS Mojave 10.14.1 beta 1 릴리즈',
-          reply: 1
-        }
-      ],
-
-      review_items: [
-        {
-          subject: '아이폰 메모리 리셋? 방법 미립자팁..',
-          reply: 3
-        },
-        {
-          subject: '일본식 갈라지는 오므라이스 만들기.',
-          reply: 20
-        },
-        {
-          subject: '영등포 신세계 올반 너무 짜네요',
-          reply: 8
-        },
-        {
-          subject: '화웨이 프리버드 5일 사용기',
-          reply: 9
-        },
-        {
-          subject: 'light for your moments, 필립스 휴 55개 시공 + 3주 사용기',
-          reply: 13
-        },
-        {
-          subject: '기어 -> 핏빕 -> 기어 -> 핏빕 사용기',
-          reply: 15
-        },
-        {
-          subject: '영알못이 원더풀 고스트 봤습니다. (스포多多)',
-          reply: 4
-        },
-        {
-          subject: '무료로 철학 공부 입문 및 팁?',
-          reply: 20
-        }
-      ],
-
-      sell_items: [
-        {
-          subject: 'USB C HUB 8-in-1'
-        },
-        {
-          subject: '(삽니다) 티모바일 공유기 ac1900'
-        },
-        {
-          subject: 'BMW e90 320i Lci 엔진보링완료'
-        },
-        {
-          subject: '애플 번들 라이트닝 케이블'
-        },
-        {
-          subject: '애플 번들 이이팟 3.5파이'
-        },
-        {
-          subject: 'G7 SKT 블랙 가개통 팝니다'
-        },
-        {
-          subject: '2017년형 맥북프로 13인치 기본형(논터치) 스페이스 그레이 팝니다'
-        },
-        {
-          subject: '이케아 사무의자, 두닷 책상 판매'
-        }
-      ],
-
-      promote_items: [
-        {
-          subject: ' [스피드통신]SK/LG/KT [긴급설치가능]/[최대 95만원]/[주야상담가능]/[ai 스피커무상]'
-        },
-        {
-          subject: '[SK/KT/LG 최고사은품 지급 ]-[ * 9월 최대 99만 ] [긴급접수][긴급입금][사후관리]'
-        },
-        {
-          subject: '단열벽지 (a-10393) 겨울을 따뜻하게 보내세요~ ^^ 간편시공 보온 결로방지 됩니다'
-        },
-        {
-          subject: '흩어져 있던 내보험.. 한손에 한번에 관리하는 똑똑한 앱!!'
-        },
-        {
-          subject: '9월 롯데카드 풍성한 신규발급 혜택, 각종제휴-통신사, 항공, 포인트, 해외결제등'
-        },
-        {
-          subject: '[9월 오토캐시백 최대 2.6%할인][신차 할부최저금리 3.3%][120개월 할부가능][신차금융'
-        },
-        {
-          subject: '[대림케어] 제휴카드 할인, 스페셜 사은품 증정, 비데, 정수기, 연수기, 양변기+세면'
-        },
-        {
-          subject: '[아우통신] 9월 3사 이벤트 , 최대 [74만원] [개통 당일지급] [Iot사은품 15만원][빠'
-        }
-      ]
     }
   }
 }
@@ -315,6 +188,13 @@ export default {
   display: block;
   width: 180px;
 }
+#list_gallery {
+  padding: 5px;
+  margin: 8px;
+  display: block;
+  border: 1px solid #D8D8D8;
+}
+
 #list {
   padding: 10px;
   margin: 5px;
