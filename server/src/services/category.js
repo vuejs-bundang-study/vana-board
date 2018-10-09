2fb37ed6d71ccd9f58bb6f2437c67e452ef43ce0
sds901234 <sds901234@naver.com>
2018-10-09 21:54:19 +0900

    config파일 수정

--- server/src/services/category.js
+++ server/src/services/category.js
@@ -8,6 +8,6 @@ export const createCategory = async (category) => {
     return Category.create(category);
 };
 
-export const deleteCategoryById = async (id) => {
-    return Category.deleteOne({ _id: id});
+export const deleteCategoryById = async (_id) => {
+    return Category.deleteOne({ _id });
 };
