f64c9875159eb5fd3b0f3298e9ce4064f504683f
sds901234 <sds901234@naver.com>
2018-10-10 00:07:01 +0900

    카테고리 수정기능 추가

--- server/src/services/category.js
+++ server/src/services/category.js
@@ -2,12 +2,14 @@ import { Category } from '../models/category';
 
 export const getCategories = async () => {
     return Category.find().populate('createdBy', 'nickname');
-    //return Category.find();
 };
 
 export const createCategory = async ({ name, createdBy }) => {
-    console.log('name : ', name, ' createdBy : ', createdBy);
     return Category.create({ name, createdBy });
+};
+
+export const updateCategoryById = async (_id, { name }) => {
+    return Category.update({ _id }, { $set: { name }});
 };
 
 export const deleteCategoryById = async (_id) => {
